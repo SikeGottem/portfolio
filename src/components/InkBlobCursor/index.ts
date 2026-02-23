a040635a19029ff867e979141125a5681dd4cfc5
Ethan Wu <ethanswu@gmail.com>
2026-02-23 11:09:07 +1100

    fix: remove stale exports from InkBlobCursor index

--- src/components/InkBlobCursor/index.ts
+++ src/components/InkBlobCursor/index.ts
@@ -1,4 +1,1 @@
 export { default as InkBlobCursor } from './InkBlobCursor';
-export type { InkBlobCursorProps } from './InkBlobCursor';
-export { DEFAULT_CONFIG } from './fluidSimulation';
-export type { FluidConfig } from './fluidSimulation';
